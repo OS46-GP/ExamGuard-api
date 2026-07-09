e92b0d7d0e729de6bd44fbb3c9ab1b5f4508f2e6
Mahmoud Ismail <mahmoudelnfiky@gmail.com>
2026-07-10 01:57:04 +0300

    Implement question CRUD

--- src/exam/question.service.ts
+++ src/exam/question.service.ts
@@ -1,24 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Question, QuestionDocument } from './schemas/question.schema';
+import { Exam, ExamDocument } from './schemas/exam.schema';
 
 @Injectable()
 export class QuestionService {
-  // TODO
-  async create(_data: any): Promise<any> {
-    return { _id: 'mock-question-id', ..._data };
+  constructor(
+    @InjectModel(Question.name) private readonly questionModel: Model<QuestionDocument>,
+    @InjectModel(Exam.name) private readonly examModel: Model<ExamDocument>,
+  ) {}
+
+  async create(data: any): Promise<QuestionDocument> {
+    const question = new this.questionModel(data);
+    await question.save();
+    await this.examModel.findByIdAndUpdate(data.examId, {
+      $push: { questionIds: question._id },
+    });
+    return question;
   }
 
-  // TODO
-  async findByExam(_examId: string): Promise<any[]> {
-    return [];
+  async findByExam(examId: string): Promise<QuestionDocument[]> {
+    return this.questionModel.find({ examId }).exec();
   }
 
-  // TODO
-  async update(_id: string, _data: any): Promise<any> {
-    return null;
+  async update(id: string, data: any): Promise<QuestionDocument> {
+    const question = await this.questionModel
+      .findByIdAndUpdate(id, data, { new: true })
+      .exec();
+    if (!question) {
+      throw new NotFoundException(`Question with id ${id} not found`);
+    }
+    return question;
   }
 
-  // TODO
-  async delete(_id: string): Promise<void> {
-    // noop
+  async delete(id: string): Promise<void> {
+    const question = await this.questionModel.findByIdAndDelete(id).exec();
+    if (!question) {
+      throw new NotFoundException(`Question with id ${id} not found`);
+    }
+    await this.examModel.findByIdAndUpdate(question.examId, {
+      $pull: { questionIds: question._id },
+    });
   }
 }
